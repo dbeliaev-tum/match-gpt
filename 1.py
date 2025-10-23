924d0f0a286102db6046fdebeba2c0d6c3b79f28
denisbelaev <d.beliaev@tum.de>
2025-10-23 12:58:31 +0200

    imported required libraries

--- 1.py
+++ 1.py
@@ -1,2 +1,16 @@
 # MatchGPT
 # AI-powered insights into your dating life success
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+import joblib
+import warnings
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+data = pd.read_csv("match_gpt_testset.csv")
