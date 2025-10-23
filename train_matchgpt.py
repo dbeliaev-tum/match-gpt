dc6f3cb4de2bb4b8c0278fcd4424c2a98197ed26
denisbelaev <d.beliaev@tum.de>
2025-10-23 13:39:58 +0200

    implemented Dataset preparation

--- train_matchgpt.py
+++ train_matchgpt.py
@@ -13,6 +13,8 @@ import warnings
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
+# --- 1. Data upload ---
+
 # Title,age,height,body_type,region_origin,sphere_of_employment,app_source,chat_channel,met,days_to_meet,sex_quality,emotional_comfort,chemistry,communication_style_match,relationship_outcome,activity
 data = pd.read_csv("match_gpt_testset.csv")
 
@@ -24,3 +26,24 @@ def process_height(height):
 
 data['height_num'] = data['height'].apply(process_height)
 data['height_num'] = data['height_num'].fillna(data['height_num'].median())
+
+# --- 2. Dataset preparation ---
+
+meet_data = data.copy()
+
+X_meet = meet_data[['age', 'height_num', 'body_type', 'region_origin',
+                    'sphere_of_employment', 'app_source',
+                    'chat_channel']]
+y_meet = meet_data['met']
+
+success_data = data[data['met'] == 1].copy()
+
+X_success = success_data[['age', 'height_num', 'body_type', 'region_origin',
+                          'sphere_of_employment', 'app_source',
+                          'chat_channel', 'activity', 'sex_quality',
+                          'emotional_comfort', 'chemistry', 'communication_style_match']]
+y_success = success_data['relationship_outcome'].apply(lambda x: 1 if x >= 1 else 0)
+
+cat_cols = ['body_type', 'region_origin',
+            'sphere_of_employment', 'app_source', 'chat_channel']
+
