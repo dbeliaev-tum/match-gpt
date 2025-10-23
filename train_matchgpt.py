cb675f782569c087feea479df9edb9dec4790d2d
denisbelaev <d.beliaev@tum.de>
2025-10-23 13:14:34 +0200

    height proccessing implemented

--- train_matchgpt.py
+++ train_matchgpt.py
@@ -13,4 +13,14 @@ import warnings
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
+# Title,age,height,body_type,region_origin,sphere_of_employment,app_source,chat_channel,met,days_to_meet,sex_quality,emotional_comfort,chemistry,communication_style_match,relationship_outcome,activity
 data = pd.read_csv("match_gpt_testset.csv")
+
+def process_height(height):
+    if isinstance(height, str) and '-' in height:
+        low, high = map(int, height.split('-'))
+        return (low + high) / 2
+    return np.nan
+
+data['height_num'] = data['height'].apply(process_height)
+data['height_num'] = data['height_num'].fillna(data['height_num'].median())
